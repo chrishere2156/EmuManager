17412ec1a9531236991c6257b41ce2f5092d414a
Chris <christopherjtemple@gmail.com>
2025-02-04 12:20:40 +0000

    Beginnings of openning main window positon, based on the screen and not pre set.

--- EmuOpen.py
+++ EmuOpen.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 
+selectScreen = tk.Tk() # this tells the program to create a window
+
 # Opens main app window apon launch
 def mainScreen():
-    selectScreen = tk.Tk() # this tells the program to create a window
-
     selectScreen.title("Emulator Selector") #sets titlebar name
     selectScreen.geometry("1280x720") #resoultion it displays in (change this later to open to a settings dependent size that user can pick)
 
@@ -15,12 +15,12 @@ def mainScreen():
     menubar.add_cascade(label ='Edit', menu = add)
     add.add_command(label ='Add Emulator', command = addEmulator)
     add.add_separator()
-    add.add_command(label ='Help', command = None)
+    add.add_command(label ='Help', command = help)
 
     #adds the 'Options' tab
     opts = tk.Menu(menubar, tearoff = 0)
     menubar.add_cascade(label ='Options', menu = opts)
-    opts.add_command(label ='Settings', command = None)
+    opts.add_command(label ='Settings', command = settings)
     opts.add_command(label ='Exit', command = selectScreen.quit)
 
     selectScreen.config(menu = menubar) #not sure but without it, it wont work lmao
@@ -50,8 +50,25 @@ def settings():
     #newWindow = 
     #pass
 
-#for the 'Help' option. only a placeholder as i hope i dont need to explain
+#for the 'Help' option. only a placeholder at the moment so i can test functions easily
 def help():
-    pass
+    winPos()
+    winSize()
+    displaySize()
+
+#x,y positon of window
+def winPos():
+    print(selectScreen.winfo_x())
+    print(selectScreen.winfo_y())
+
+#size of main program window
+def winSize():
+    print(selectScreen.winfo_width())
+    print(selectScreen.winfo_height())
+
+#display size of monitor. this is seems incorrect but i only find out when i implement more.
+def displaySize():
+    print(selectScreen.winfo_screenwidth())
+    print(selectScreen.winfo_screenheight())
 
 mainScreen() #runs whole program
